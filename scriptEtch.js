0adb78ee8fd33df984f5409c0fb198f854e0c98a
Chris Martine <cmmartine@gmail.com>
2022-09-29 14:53:13 -0400

    Created the changeBlockColor function to change each block color on mouse over

--- scriptEtch.js
+++ scriptEtch.js
@@ -1,20 +1,32 @@
 const blockContainer = document.getElementById('blockContainer');
+let newBlock;
 
-//Using a block CONTAINER size of 960, createBlock auto sizes the blocks depending how many there are, and align them in an even box
+//Using a block CONTAINER size of 960x960px, createBlock auto sizes the blocks depending how many there are, and align them in an even box
 function createBlock(number){
     for (let i = 0; i < number*number; i++ ){
         if(number > 100){
             alert('Error, number entered cannot be greater than 100.')
             return;
-        }
-        const newBlock = document.createElement('div');
+        } else{
+        newBlock = document.createElement('div');
         newBlock.classList.add('block');
         const blocksize = 960 / number;
         blockContainer.appendChild(newBlock);
         blockContainer.style.gridTemplateRows =  'repeat(' + number +', ' + blocksize + 'px)';
         blockContainer.style.gridTemplateColumns =  'repeat(' + number +', ' + blocksize + 'px)';
+        }
     }
-    const blocks = document.querySelectorAll('block');
+
+    changeBlockColor();
 };
 
-
+function changeBlockColor(){
+    const changeColor = document.getElementsByClassName('block');
+    for(let j=0; j < changeColor.length; j++){
+    changeColor[j].addEventListener('mouseover', function(){
+     changeColor[j].style.backgroundColor = 'Black';
+    });
+    }
+  };
+  
+ 
