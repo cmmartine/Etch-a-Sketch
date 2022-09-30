074feb9d9c3658059188da921983edb4e43c9359
Chris Martine <cmmartine@gmail.com>
2022-09-30 08:48:28 -0400

    Added a button that removes old grid if any and asks user for a number to make a new grid

--- scriptEtch.js
+++ scriptEtch.js
@@ -4,10 +4,6 @@ let newBlock;
 //Using a block CONTAINER size of 960x960px, createBlock auto sizes the blocks depending how many there are, and align them in an even box
 function createBlock(number){
     for (let i = 0; i < number*number; i++ ){
-        if(number > 100){
-            alert('Error, number entered cannot be greater than 100.')
-            return;
-        } else{
         newBlock = document.createElement('div');
         newBlock.classList.add('block');
         const blocksize = 960 / number;
@@ -15,10 +11,10 @@ function createBlock(number){
         blockContainer.style.gridTemplateRows =  'repeat(' + number +', ' + blocksize + 'px)';
         blockContainer.style.gridTemplateColumns =  'repeat(' + number +', ' + blocksize + 'px)';
         }
-    }
-
+    
     changeBlockColor();
-};
+}
+
 
 function changeBlockColor(){
     const changeColor = document.getElementsByClassName('block');
@@ -27,6 +23,24 @@ function changeBlockColor(){
      changeColor[j].style.backgroundColor = 'Black';
     });
     }
-  };
+};
   
+
+const gridButton = document.getElementById('btn');
+gridButton.addEventListener('click', function(){
+    //grab newBlock from createBlock function to remove any created blocks
+    newBlock = document.querySelectorAll('.block');
+    newBlock.forEach(blocks => {
+        blocks.remove();
+    });
+    //change size of grid
+    let userInputNumber = prompt("Please enter a new number between 1 and 100 to specify the amount of both columns and rows in the new grid:");
+    let userNumber = parseInt(userInputNumber);
+    console.log(typeof userNumber);
+    if(userNumber <= 0 || userNumber > 100 || typeof userNumber != 'number'){
+        userInputNumber = prompt("Please enter a new number between 1 and 100 to specify the amount of both columns and rows in the new grid:");
+        userNumber = parseInt(userInputNumber);
+    }
+    createBlock(userNumber);
+});
  
